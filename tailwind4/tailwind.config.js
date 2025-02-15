ccc6ad6e486b58546d671573ff62dcb850373770
arash <arash619551@gmail.com>
2025-02-15 13:23:57 +0330

    Add files via upload

---
+++ tailwind4/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./*.html", "./**/*.html",],
+    theme: {
+        extend: {
+            colors: {
+                color1: "#e5bf4a",
+                color2: "#fff",
+                color3: "#0a0a0a",
+                color4: "#4c4c4c"
+            },
+        },
+    },
+    plugins: [],
+};
