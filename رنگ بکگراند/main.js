45f83c2dabaf21bd39185ffb1d4e8d016cba09b3
arash <arash619551@gmail.com>
2025-02-15 12:53:27 +0330

    Add files via upload

---
+++ رنگ بکگراند/main.js
@@ -0,0 +1,15 @@
+function blue() {
+    document.body.style.backgroundColor = "blue";
+}
+function red() {
+    document.body.style.backgroundColor = "red";
+}
+function green() {
+    document.body.style.backgroundColor = "green";
+}
+function white() {
+    document.body.style.backgroundColor = "white";
+}
+function pink() {
+    document.body.style.backgroundColor = "pink";
+}
