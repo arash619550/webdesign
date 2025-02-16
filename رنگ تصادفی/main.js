f5d0fd3284af3260d680f1790b3967d0ba5f4a58
arash <arash619551@gmail.com>
2025-02-16 14:00:04 +0330

    Add files via upload

---
+++ رنگ تصادفی/main.js
@@ -0,0 +1,41 @@
+
+/* توضیحات : چون فقط یک بار میتوانستم رنگ ایجاد کنم متغیر ها را داخل تابع بردم */
+function random() {
+    const r1 = Number(Math.floor(Math.random() * 256));
+    const g1 = Number(Math.floor(Math.random() * 256));
+    const b1 = Number(Math.floor(Math.random() * 256));
+    document.getElementById("id1").style.backgroundColor = "rgb(" + r1 + ", " + g1 + ", " + b1 + ")";
+    const r2 = Number(Math.floor(Math.random() * 256));
+    const g2 = Number(Math.floor(Math.random() * 256));
+    const b2 = Number(Math.floor(Math.random() * 256));
+    document.getElementById("id2").style.backgroundColor = "rgb(" + r2 + ", " + g2 + ", " + b2 + ")";
+    const r3 = Number(Math.floor(Math.random() * 256));
+    const g3 = Number(Math.floor(Math.random() * 256));
+    const b3 = Number(Math.floor(Math.random() * 256));
+    document.getElementById("id3").style.backgroundColor = "rgb(" + r3 + ", " + g3 + ", " + b3 + ")";
+    const r4 = Number(Math.floor(Math.random() * 256));
+    const g4 = Number(Math.floor(Math.random() * 256));
+    const b4 = Number(Math.floor(Math.random() * 256));
+    document.getElementById("id4").style.backgroundColor = "rgb(" + r4 + ", " + g4 + ", " + b4 + ")";
+    const r5 = Number(Math.floor(Math.random() * 256));
+    const g5 = Number(Math.floor(Math.random() * 256));
+    const b5 = Number(Math.floor(Math.random() * 256));
+    document.getElementById("id5").style.backgroundColor = "rgb(" + r5 + ", " + g5 + ", " + b5 + ")";
+    const r6 = Number(Math.floor(Math.random() * 256));
+    const g6 = Number(Math.floor(Math.random() * 256));
+    const b6 = Number(Math.floor(Math.random() * 256));
+    document.getElementById("id6").style.backgroundColor = "rgb(" + r6 + ", " + g6 + ", " + b6 + ")";
+    const r7 = Number(Math.floor(Math.random() * 256));
+    const g7 = Number(Math.floor(Math.random() * 256));
+    const b7 = Number(Math.floor(Math.random() * 256));
+    document.getElementById("id7").style.backgroundColor = "rgb(" + r7 + ", " + g7 + ", " + b7 + ")";
+    const r8 = Number(Math.floor(Math.random() * 256));
+    const g8 = Number(Math.floor(Math.random() * 256));
+    const b8 = Number(Math.floor(Math.random() * 256));
+    document.getElementById("id8").style.backgroundColor = "rgb(" + r8 + ", " + g8 + ", " + b8 + ")";
+    const r9 = Number(Math.floor(Math.random() * 256));
+    const g9 = Number(Math.floor(Math.random() * 256));
+    const b9 = Number(Math.floor(Math.random() * 256));
+    document.getElementById("id9").style.backgroundColor = "rgb(" + r9 + ", " + g9 + ", " + b9 + ")";
+}
+
